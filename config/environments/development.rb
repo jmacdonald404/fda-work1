e48aa968688aa8887e354f171abf0e9374e976b0
jmacdonald404 <jmacdonald404@gmail.com>
2025-03-06 17:26:33 +0000

    add dev server to allowed hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # add dev server to allowed hosts
+  config.hosts << "dev.lain.ch:3000"
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
