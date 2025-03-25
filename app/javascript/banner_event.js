a7a6df2b6078cc653deb3ce38079c2226d6f4df3
jmacdonald404 <jmacdonald404@gmail.com>
2025-03-25 22:03:51 +0000

    add rendered page check for banner event

--- app/javascript/banner_event.js
+++ app/javascript/banner_event.js
@@ -1,7 +1,10 @@
-const bannerEvent = document.getElementById('banner_event');
-bannerEvent.addEventListener('click', function(e) {
-  e.stopImmediatePropagation();
-  e.preventDefault();
-  $('#bannerModal').modal('show');
-  $('.modal-backdrop.show').attr('style', 'opacity: 0.95 !important; background-color: rgba(45, 57, 88, 0.85) !important;');
-});
+if(!document.querySelector("#loginSubmit")&&(!document.querySelector("#signUpSubmit"))) {
+  console.log('loading banner event')
+  const bannerEvent = document.getElementById('banner_event');
+  bannerEvent.addEventListener('click', function(e) {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    $('#bannerModal').modal('show');
+    $('.modal-backdrop.show').attr('style', 'opacity: 0.95 !important; background-color: rgba(45, 57, 88, 0.85) !important;');
+  });
+}
