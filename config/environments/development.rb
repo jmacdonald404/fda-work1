d70e032ff1f6596678870f444366aaa06bc6f463
jmacdonald404 <jmacdonald404@gmail.com>
2025-03-25 21:21:06 +0000

    tweak dev hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # config.force_ssl = false
   # add dev server to allowed hosts
   config.hosts << /[a-z0-9\-]+\.fly\.dev/
   config.hosts << /[a-z0-9\-]+\.fly\.io/
