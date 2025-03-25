0eb2726829fcdd2e84a887ee9287f87c16bc3c48
jmacdonald404 <jmacdonald404@gmail.com>
2025-03-25 21:20:04 +0000

    debug handling tippy (not) loading

--- app/javascript/add_tippy.js
+++ app/javascript/add_tippy.js
@@ -8,10 +8,15 @@ import tippy from 'tippy.js';
 import { TempusDominus } from '@eonasdan/tempus-dominus';
 window.tippy = tippy;
 
+console.log('begin loading tippy, checking what page is rendered');
+if(!document.querySelector("#loginSubmit")&&(!document.querySelector("#signUpSubmit"))) {
+  console.log('not on signup or login, continuing to load tippy');
 
-if(!document.querySelector("#loginSubmit")&&(!document.querySelector("#signUpSubmit"))){
+
   const template = document.getElementById('tooltip-select2');
   const template2 = document.getElementById('tooltip-unavailable');
+  console.log(template.nodeType);
+  console.log(template2.nodeType);
   template.style.display = template2.style.display = "block";
   tippy(document.getElementById('tt-unavail'), {
     maxWidth: 483,
@@ -25,12 +30,19 @@ if(!document.querySelector("#loginSubmit")&&(!document.querySelector("#signUpSub
     theme: 'main',
     content: template
   });
+  console.log('tippy should be correctly loaded at this point');
+} else {
+  console.log('on signup or login page, not loading tippy');
 }
+
+
 
 // window.tempusDominus = TempusDominus;
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('domloaded');
+
+
   const element = document.getElementById('datetimepicker1');
   
   if (element) {
